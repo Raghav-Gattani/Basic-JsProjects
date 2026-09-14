7695ebc84594e29f19983db67369849838e0462c
Raghav-Gattani <raghavgattani007@gmail.com>
2026-09-14 23:36:44 +0530

    Update maskEmail.js

--- maskEmail.js
+++ maskEmail.js
@@ -9,3 +9,4 @@ const maskEmail = (email) => {
 let email = 'itismyfirstproject@javascript.com'
 console.log(maskEmail(email));
 // This is my first JavaScript project.
+// this can be used in the future just by changing some function.
